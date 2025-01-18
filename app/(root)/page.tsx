41cef2880dcbfdfed1e0d9eece6a577d37ac14c1
prathamesh-pichkate <prathameshpichkate@gmail.com>
2025-01-18 14:31:17 +0530

    Added the files

---
+++ app/(root)/page.tsx
@@ -0,0 +1,46 @@
+import SearchForm from "@/components/SearchForm";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+  const params = {search:query || null};
+
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
+
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect With Entrepreneurs
+        </h1>
+        <p>
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competition
+        </p>
+        <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Showing results for "${query}"` : "All latest Startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No results found</p>
+          )}
+        </ul>
+      </section>
+      <SanityLive />
+    </>
+  );
+}
